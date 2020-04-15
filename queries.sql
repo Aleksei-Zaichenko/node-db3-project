3a4ed7632b26969a59f925f36fafe3a4a155a31a
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-15 19:59:37 -0400

    Completed SQL statements for northwind.db3

--- queries.sql
+++ queries.sql
@@ -1,9 +1,27 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+SELECT Id as ProductID, ProductName, CategoryName FROM Product
+JOIN Category
+ON Product.CategoryId = Category.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT Id as OrderID, CompanyName FROM [Order] as Orders
+JOIN Shipper
+ON Orders.ShipVia = Shipper.Id
+WHERE Orders.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+SELECT ProductName, QuantityPerUnit FROM OrderDetail
+JOIN Product
+ON OrderDetail.ProductId = Product.Id
+WHERE OrderDetail.OrderId = 10251
+ORDER BY ProductName;
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+SELECT Id, CompanyName as CustomerCompanyName, LastName as EmployeeLastName
+FROM [Order] as Orders
+JOIN [Customer]
+ON Orders.CustomerId = Customer.Id
+JOIN [Employee]
+ON Orders.EmployeeId = Employee.Id;
